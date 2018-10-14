831228efcfa16fbdb63a2f1f2579dd193361bfb3
Jack Spence <jack.c.spence@gmail.com>
2018-10-14 13:56:56 -0400

    Added doxygen comments.

--- Dijkstra.h
+++ Dijkstra.h
@@ -15,6 +15,12 @@
  */
 ssize_t Dijkstra_path(const Graph *g, const char *start, const char *end, char ***path);
 
+/**
+ * @brief Changes 2d array of the maze and changes the route to show the solved maze
+ * @param mazeFromFile 2d array of maze
+ * @param route Nodes in graph that lead to end
+ * @param hops Number of hops for tracking in loop
+ */
 void Dijkstra_solveMaze(char **mazeFromFile, char **route, size_t hops);
 
 #endif
