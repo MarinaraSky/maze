be6eb1dccec45433bc2cd1869362575d7e20145c
Jack Spence <jack.c.spence@gmail.com>
2018-10-14 13:57:11 -0400

    Added doxygen comments.

--- graph/GraphSerializer.h
+++ graph/GraphSerializer.h
@@ -12,6 +12,9 @@ GraphSerializer_toStdout(const Graph *g);
 /**
  * @brief Deserializes Graph object from input
  * @param fp FILE * to read serialized Graph from
+ * @param mazeFromFile char *** to make the map read be available in main
+ * @param maxLineLength Tracking for mazeFromFile size (X axis)
+ * @param lineCount Tracking for mazeFromFile size (Y axis)
  * @return Graph object (NULL on error)
  */
 Graph *
