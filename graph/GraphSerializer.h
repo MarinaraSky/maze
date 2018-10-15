b4acdbba4ed12f60b312db5c1d69c5f2d5e594c9
Jack Spence <jack.c.spence@gmail.com>
2018-10-14 20:51:16 -0400

    Functionized creating array to be used for map generation not from a file.

--- graph/GraphSerializer.h
+++ graph/GraphSerializer.h
@@ -15,9 +15,21 @@ GraphSerializer_toStdout(const Graph *g);
  * @param mazeFromFile char *** to make the map read be available in main
  * @param maxLineLength Tracking for mazeFromFile size (X axis)
  * @param lineCount Tracking for mazeFromFile size (Y axis)
+ * @param flags Used to keep track of CLI arguments
  * @return Graph object (NULL on error)
  */
 Graph *
 GraphSerializer_fromFile(FILE *fp, char ***mazeFromFile, size_t *maxLineLength, size_t *lineCount, char flags);
+
+/**
+ * @brief Taking a 2d array will make nodes from each available space
+ * @param g Graph ** to modify and produce graph from maze
+ * @param mazeFromFile char *** to parse and make the graph
+ * @param maxLineLength Tracking for mazeFromFile size (X axis)
+ * @param lineCount Tracking for mazeFromFile size (Y axis)
+ * @param flags Used to keep track of CLI arguments
+ */
+void 
+GraphSerializer_fromMaze(Graph **g, char **maze, size_t *maxLength, size_t *lineCount, char flags);
 
 #endif
